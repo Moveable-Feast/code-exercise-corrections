f5038c7f378d3fc1d9b9eb617fe50aefd3d5f13e
LDDSY <100818596+Moveable-Feast@users.noreply.github.com>
2026-01-20 09:28:03 +0000

    new file:   "Python/LeetCode/#136_\345\217\252\345\207\272\347\216\260\344\270\200\346\254\241\347\232\204\346\225\260\345\255\227.ipynb" 	new file:   "Python/LeetCode/#240_\346\220\234\347\264\242\344\272\214\347\273\264\347\237\251\351\230\265_II.ipynb" 	new file:   "Python/LeetCode/#295!_\346\225\260\346\215\256\346\265\201\347\232\204\344\270\255\344\275\215\346\225\260.ipynb" 	new file:   "Python/LeetCode/#3_\346\227\240\351\207\215\345\244\215\345\255\227\347\254\246\347\232\204\346\234\200\351\225\277\345\255\220\344\270\262.ipynb" 	new file:   "Python/LeetCode/#41_\347\274\272\345\244\261\347\232\204\347\254\254\344\270\200\344\270\252\346\255\243\346\225\260.ipynb" 	new file:   "Python/LeetCode/#4_\345\257\273\346\211\276\344\270\244\344\270\252\346\255\243\345\272\217\346\225\260\347\273\204\347\232\204\344\270\255\344\275\215\346\225\260.ipynb" 	new file:   "Python/LeetCode/#53!_\346\234\200\345\244\247\345\255\220\346\225\260\347\273\204\345\222\214.ipynb" 	new file:   "Python/LeetCode/#54_\350\236\272\346\227\213\347\237\251\351\230\265.ipynb" 	new file:   "Python/LeetCode/#994_\350\205\220\347\203\202\347\232\204\346\251\230\345\255\220.ipynb" 	new file:   Python/LeetCode/template.ipynb 	modified:   Python/script.py

--- Python/script.py
+++ Python/script.py
@@ -1,2 +1,15 @@
-list = [1, 2, 3, 4, 5]
-print(list)
+nums = [1, 2, 3, 4, 5, 6, 7]
+n = len(nums)
+k = 3
+
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(k):
+            nums = [nums[n - 1]] + nums[0 : n - 1]
+
+solution = Solution()
+solution.rotate(nums, k)
