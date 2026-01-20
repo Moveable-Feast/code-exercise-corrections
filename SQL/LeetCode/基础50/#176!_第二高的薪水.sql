c3ff1a9ed7bbbc00e72b247300e4af0ff28868e3
LDDSY <100818596+Moveable-Feast@users.noreply.github.com>
2026-01-20 18:51:39 +0000

    renamed:    "SQL/SQL50\345\237\272\347\241\200/2_\350\277\236\346\216\245/#1084!_\351\224\200\345\224\256\345\210\206\346\236\220_III.sql" -> "SQL/LeetCode/\345\237\272\347\241\20050/#1084!_\351\224\200\345\224\256\345\210\206\346\236\220_III.sql" 	renamed:    "SQL/SQL50\345\237\272\347\241\200/2_\350\277\236\346\216\245/#1164_\346\214\207\345\256\232\346\227\245\346\234\237\347\232\204\344\272\247\345\223\201\344\273\267\346\240\274.sql" -> "SQL/LeetCode/\345\237\272\347\241\20050/#1164_\346\214\207\345\256\232\346\227\245\346\234\237\347\232\204\344\272\247\345\223\201\344\273\267\346\240\274.sql" 	renamed:    "SQL/SQL50\345\237\272\347\241\200/2_\350\277\236\346\216\245/#1280_\345\255\246\347\224\237\344\273\254\345\217\202\345\212\240\345\220\204\347\247\221\346\265\213\350\257\225\347\232\204\346\254\241\346\225\260.sql" -> "SQL/LeetCode/\345\237\272\347\241\20050/#1280_\345\255\246\347\224\237\344\273\254\345\217\202\345\212\240\345\220\204\347\247\221\346\265\213\350\257\225\347\232\204\346\254\241\346\225\260.sql" 	renamed:    "SQL/SQL50\345\237\272\347\241\200/2_\350\277\236\346\216\245/#1581_\350\277\233\345\272\227\345\215\264\346\234\252\350\277\233\350\241\214\350\277\207\344\272\244\346\230\223\347\232\204\351\241\276\345\256\242.sql" -> "SQL/LeetCode/\345\237\272\347\241\20050/#1581_\350\277\233\345\272\227\345\215\264\346\234\252\350\277\233\350\241\214\350\277\207\344\272\244\346\230\223\347\232\204\351\241\276\345\256\242.sql" 	renamed:    "SQL/SQL50\345\237\272\347\241\200/7_\351\253\230\347\272\247\345\255\227\347\254\246\344\270\262\345\207\275\346\225\260\343\200\201\346\255\243\345\210\231\350\241\250\350\276\276\345\274\217 \343\200\201\345\255\220\345\217\245/#1667!_\344\277\256\345\244\215\350\241\250\344\270\255\347\232\204\345\220\215\345\255\227.sql" -> "SQL/LeetCode/\345\237\272\347\241\20050/#1667!_\344\277\256\345\244\215\350\241\250\344\270\255\347\232\204\345\220\215\345\255\227.sql" 	new file:   "SQL/LeetCode/\345\237\272\347\241\20050/#176!_\347\254\254\344\272\214\351\253\230\347\232\204\350\226\252\346\260\264.sql" 	renamed:    "SQL/SQL50\345\237\272\347\241\200/3_\350\201\232\345\220\210\345\207\275\346\225\260/#550_\346\270\270\346\210\217\347\216\251\346\263\225\345\210\206\346\236\220_IV.sql" -> "SQL/LeetCode/\345\237\272\347\241\20050/#550_\346\270\270\346\210\217\347\216\251\346\263\225\345\210\206\346\236\220_IV.sql" 	new file:   "SQL/LeetCode/\350\277\233\351\230\26650/leave_it" 	new file:   "SQL/Runnob/MySQL\346\225\231\347\250\213.sql" 	new file:   "SQL/Runnob/PostgreSQL\346\225\231\347\250\213.sql" 	new file:   "SQL/Runnob/PostgreSQL\351\253\230\347\272\247.sql" 	new file:   "SQL/\343\200\212Oracle 11g\346\225\260\346\215\256\345\272\223\345\237\272\347\241\200\346\225\231\347\250\213\343\200\213/chapter_9/9_2.sql"

---
+++ SQL/LeetCode/基础50/#176!_第二高的薪水.sql
@@ -0,0 +1,9 @@
+-- 解答
+select t.SecondHighestSalary from
+  (select t1.sal SecondHighestSalary, rownum rn from
+    select * from emp order by sal desc) t1
+  where rownum <= 2) t
+where rn >= 2;
+-- 答案
+select max(salary) SecondHighestSalary from Employee
+where salary < (select max(salary) from Employee);
