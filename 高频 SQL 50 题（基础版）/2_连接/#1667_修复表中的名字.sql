e9b3723ce0c71de1131fe3caf9f19a21807ebe0e
LDDSY <100818596+Moveable-Feast@users.noreply.github.com>
2026-01-20 17:35:54 +0800

    Delete 高频 SQL 50 题（基础版） directory

--- 高频 SQL 50 题（基础版）/2_连接/#1667_修复表中的名字.sql
+++
@@ -1,9 +0,0 @@
--- 解答
--- Oracle
-select user_id, initcap(name) name from users order by user_id;
-
--- 答案
-select
-  user_id,
-  concat(upper(substring(name, 1, 1)), lower(substring(name, 2))) name
-from users order by user_id;
--- 高频 SQL 50 题（基础版）/7_高级字符串函数、正则表达式 、子句/#1667!_修复表中的名字.sql
+++
@@ -1,4 +0,0 @@
-select
-  user_id,
-  concat(upper(substring(name, 1, 1)), lower(substring(name, 2))) name
-from users order by user_id;
