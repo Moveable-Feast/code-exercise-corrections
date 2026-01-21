3c7f756b1fb7dbface79faaae04d620fb55ca239
LDDSY <100818596+Moveable-Feast@users.noreply.github.com>
2026-01-21 05:00:27 +0000

    new file:   "Python/LeetCode/\347\203\255\351\242\230100/#189_\350\275\256\350\275\254\346\225\260\347\273\204.ipynb" 	modified:   Python/script.py

--- Python/script.py
+++ Python/script.py
@@ -1,15 +1,6 @@
-nums = [1, 2, 3, 4, 5, 6, 7]
-n = len(nums)
-k = 3
-
-class Solution:
-    def rotate(self, nums: list[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        for i in range(k):
-            nums = [nums[n - 1]] + nums[0 : n - 1]
-
-solution = Solution()
-solution.rotate(nums, k)
+nums = [1,2,3]
+nums_old = nums
+nums_new = nums.copy()
+nums_new = [4,5,6]
+nums = nums_new.copy()
+print(nums, nums_new, nums_old)
